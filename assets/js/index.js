c5e2c51924495205d310d3f23f90460b92dacc52
Jhon Cordova <jacordova838@gmail.com>
2025-02-13 03:40:17 -0500

    some improvements

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,4 @@
-// Cierra el menú Offcanvas cuando se hace clic en un enlace
+// Cierre del offcanvas al hacer click en un enlace
 document.addEventListener("DOMContentLoaded", function () {
   var offcanvasElement = document.getElementById("offcanvasNavbar");
   var navLinks = offcanvasElement.querySelectorAll(".nav-link");
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-// Añade animación infinita a elementos scroller
+// Animacion infinita
 const scrollers = document.querySelectorAll(".scroller");
 
 if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
@@ -34,62 +34,4 @@ function addAnimation() {
     });
   });
 }
-
-// Envía un mensaje por WhatsApp con los datos del formulario
-function enviarWhatsapp(event) {
-  event.preventDefault();
-
-  const name = document.getElementById("name").value;
-  const email = document.getElementById("email").value;
-  const mensaje = document.getElementById("mensaje").value;
-
-  const url =
-    `https://api.whatsapp.com/send?phone=957995839&text=Hola, me llamo ${name}.\n` +
-    ` Mi correo electrónico es ${email} y ${mensaje}`;
-
-  if (name === "" || email === "" || mensaje === "") {
-    alert("Por favor, completa todos los campos.");
-    return;
-  }
-
-  window.open(url, "_blank");
-}
-
-// Añade la clase "active" a secciones visibles usando Intersection Observer
-document.addEventListener("DOMContentLoaded", function () {
-  const sections = document.querySelectorAll("section");
-
-  const observer = new IntersectionObserver(
-    (entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          entry.target.classList.add("active");
-          observer.unobserve(entry.target);
-        }
-      });
-    },
-    {
-      threshold: 0.1,
-    }
-  );
-
-  sections.forEach((section) => {
-    observer.observe(section);
-  });
-});
-
-/*
-// Cambia el estilo del navbar al hacer scroll
-document.addEventListener("DOMContentLoaded", function () {
-  const navbar = document.querySelector(".navbar");
-
-  window.addEventListener("scroll", function () {
-    if (window.scrollY >= 1) {
-      navbar.classList.add("scrolled");
-    } else {
-      navbar.classList.remove("scrolled");
-    }
-  });
-});
-*/
 
