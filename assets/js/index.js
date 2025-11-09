1b1c82ddc1efc44acd9c2d5afa222d11de6d4d7a
Jhon Cordova <jacordova838@gmail.com>
2025-11-08 21:49:45 -0500

    modal-proyects

--- assets/js/index.js
+++ assets/js/index.js
@@ -13,28 +13,6 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-// Animacion infinita
-const scrollers = document.querySelectorAll(".scroller");
-
-if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
-  addAnimation();
-}
-
-function addAnimation() {
-  scrollers.forEach((scroller) => {
-    scroller.setAttribute("data-animated", true);
-
-    const scrollerInner = scroller.querySelector(".scroller__inner");
-    const scrollerContent = Array.from(scrollerInner.children);
-
-    scrollerContent.forEach((item) => {
-      const duplicatedItem = item.cloneNode(true);
-      duplicatedItem.setAttribute("aria-hidden", true);
-      scrollerInner.appendChild(duplicatedItem);
-    });
-  });
-}
-
 //Lenguajes
 const langButtons = document.querySelectorAll("[data-language]");
 const textsToChange = document.querySelectorAll("[data-section]");
